1acce8d50ad3ce5562d3c44e09bd9b8b9824c750
Rajeev Krishna <lambo2572@gmail.com>
2023-06-15 23:59:34 +0530

    non-responsive landing page made

--- go-gutch/src/app/page.js
+++ go-gutch/src/app/page.js
@@ -1,11 +1,18 @@
 // import Image from 'next/image'
+// import link from 'next/Link'
 import {Navbar} from '../components/Navbar'
+import { Hero } from '@/components/Hero'
+// import { Footer } from '@/components/Footer'
+// import {SignUp} from './components/SignUp'
+// import {useState} from 'react'
 
 export default function Home() {
-  // const [darkMode, useDarkMode] = useState(false)
+  // const [darkMode, useDarkMode] = useState(false);
   return (
-    <div>
-      <Navbar />
+    <div className='bg-white md:h-screen xs:h-full text-black'>
+      <Navbar className=""/>
+      <Hero />
+      {/* <Footer /> */}
     </div>
   )
 }
---
+++ go-gutch/src/components/signup.js
@@ -0,0 +1,9 @@
+import {Link} from 'next/link'
+
+export const SignUp = () => {
+    return (
+        <div className="form">
+            <h1>Sign Up</h1>
+        </div>
+    )
+}
